bc1d4f2e386be395feb0fc6ba5e2fe5e4222979e
winplay <winplay@winplay.io>
2019-12-31 14:32:46 +0800

    build
    Change-Id: Ice5a950dc14f42577dfb025bf4f74928b4d6eabf

---
+++ precache-manifest.70279dace7123be7c4d22b75003989fb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c8d25b80b9c289e868",
+    "url": "/static/css/main.4405f7a2.chunk.css"
+  },
+  {
+    "revision": "48c8d25b80b9c289e868",
+    "url": "/static/js/main.48c8d25b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "71cb1f59232309ae9d9a",
+    "url": "/static/js/2.71cb1f59.chunk.js"
+  },
+  {
+    "revision": "3349abff9e655d7b543756d875ce9852",
+    "url": "/static/media/card_back.3349abff.png"
+  },
+  {
+    "revision": "f4416783350aaf3193a7d582ff5230ca",
+    "url": "/static/media/felt_bg.f4416783.png"
+  },
+  {
+    "revision": "044347fe7bb2c0696e0efc64c00ebc06",
+    "url": "/static/media/wp-bancor.044347fe.png"
+  },
+  {
+    "revision": "408ad750652e8358ec6586e14b0f0c64",
+    "url": "/static/media/lotto.408ad750.png"
+  },
+  {
+    "revision": "bff5e8d97bad59458d48bc0fabd6aa38",
+    "url": "/static/media/dice.bff5e8d9.png"
+  },
+  {
+    "revision": "c9c70e822c944a7a38886ba154eb0103",
+    "url": "/static/media/poker.c9c70e82.png"
+  },
+  {
+    "revision": "c4cd6642c20f6473ef5b38fbf9c3aa11",
+    "url": "/static/media/en.c4cd6642.svg"
+  },
+  {
+    "revision": "679d5ec38d71530f840d78633848bd13",
+    "url": "/static/media/cn.679d5ec3.svg"
+  },
+  {
+    "revision": "ab471b720ea880cbeb6a5b8734d0c3e2",
+    "url": "/index.html"
+  }
+];
